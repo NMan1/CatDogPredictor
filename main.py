8165cfa5f92207db5f8e2d787665498f4d42cb4f
NMan1 <nickmantini01@gmail.com>
2020-05-04 13:04:10 -0500

    Added a bunch of saves and logs

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import os
 import cv2
 import random
 import pickle
+import time
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
 import tensorflow as tf
+import tensorflow.python.util.deprecation as deprecation
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.python.client import device_lib
 
+dense_layers = [0]
+layer_sizes = [64]
+conv_layers = [3]
 
 # set to your directory
 datadir = "C:\\Users\\nickm\\PycharmProjects\\CatDogPredictor\\PetImages"
@@ -19,6 +26,7 @@ load_data = True
 load_model = False
 x = [] # features
 y = [] # labels
+deprecation._PRINT_DEPRECATION_WARNINGS = False
 
 
 def load_training_data():
@@ -30,6 +38,9 @@ def load_training_data():
         x = pickle.load(file)
     with open("y_pickle", "rb") as file:
         y = pickle.load(file)
+    # normalize
+    x = np.array(x / 255.0)
+    y = np.array(y)
 
 
 def create_training_data():
@@ -39,10 +50,10 @@ def create_training_data():
         class_num = categories.index(caetegoir)
         for img in os.listdir(path):
             try:
-                img_array =  cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
                 new_array = cv2.resize(img_array, (img_size, img_size))
                 training_data.append([new_array, class_num])
-            except Exception as e:
+            except Exception:
                 os.remove(path+"\\"+img)
 
     for features, label in training_data:
@@ -67,7 +78,7 @@ if load_data:
 
 # load model
 if load_model:
-    model = tf.keras.models.load_model('saved_model/saved_model.pb')
+    model = tf.keras.models.load_model('saves/saved_model/')
 
 
 # create data
@@ -80,27 +91,34 @@ if not load_data:
 
 
 # create model
-if not load_model:
-    model = Sequential()
+if not load_model:      
+    for dense_layer in dense_layers:
+        for layer_size in layer_sizes:
+            for conv_layer in conv_layers:
+                    name = f"conv-{conv_layer}-nodes-{layer_size}-dense-{dense_layer}"
+                    tensor_board = TensorBoard(log_dir=f'logs\\{name}')
 
-    model.add(Conv2D(64, (3, 3), input_shape=x.shape[1:]))
-    model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+                    model = Sequential()
 
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+                    model.add(Conv2D(layer_size, (3, 3), input_shape=x.shape[1:]))
+                    model.add(Activation("relu"))
+                    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Activation("relu"))
+                    for l in range(conv_layer-1):
+                        model.add(Conv2D(layer_size, (3, 3)))
+                        model.add(Activation("relu"))
+                        model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Dense(1))
-    model.add(Activation("sigmoid"))
+                    model.add(Flatten())
+                    for l in range(dense_layer):
+                        model.add(Dense(layer_size))
+                        model.add(Activation("relu"))
 
-    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
-    model.fit(x, y, batch_size=32, epochs=3, validation_split=0.1)
+                    model.add(Dense(1))
+                    model.add(Activation("sigmoid"))
 
-    # save model
-    model.save('saved_model/')
+                    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+                    model.fit(x, y, batch_size=32, epochs=10, validation_split=0.1, callbacks=[tensor_board])
 
+                    # save model
+                    model.save(f'saves/saved-model-{name}/')
